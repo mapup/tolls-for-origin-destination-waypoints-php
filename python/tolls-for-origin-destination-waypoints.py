2dc76d43f517d4b5dbb6e24c12c81818e5f04b00
Abhinav Saini <abhinavs@mapup.ai>
2024-03-08 13:09:23 +0530

    refactor: python

--- python/tolls-for-origin-destination-waypoints.py
+++ python/tolls-for-origin-destination-waypoints.py
@@ -6,14 +6,29 @@ TOLLGURU_API_KEY = os.environ.get("TOLLGURU_API_KEY")
 TOLLGURU_API_URL = "https://apis.tollguru.com/toll/v2"
 ORIGIN_DESTINATION_ENDPOINT = "origin-destination-waypoints"
 
+# From and To locations
+source = "Philadelphia, PA, USA"
+destination = "New York, NY, USA"
+
+# Explore https://tollguru.com/toll-api-docs to get best of all the parameter that TollGuru has to offer
+request_parameters = {
+    "vehicle": {
+        "type": "2AxlesAuto"
+    },
+    # Visit https://en.wikipedia.org/wiki/Unix_time to know the time format
+    "departure_time": "2021-01-05T09:46:08Z",
+}
+
 headers = {"x-api-key": TOLLGURU_API_KEY, "Content-Type": "application/json"}
 
 body = {
-    "from": {"address": "HWY-402 Point Edward, ON N7V Canada"},
-    "to": {"address": "I-94 Port Huron, MI 48060 USA"},
-    "vehicle": {
-        "type": "2AxlesAuto",
+    "from": {
+        "address": source
     },
+    "to": {
+        "address": destination
+    },
+    **request_parameters,
 }
 
 response = requests.post(
